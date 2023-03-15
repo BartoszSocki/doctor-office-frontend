9f1f63076653ee9373523ee3d89e026ddc75df1e
Bartosz S <01169625@pw.edu.pl>
2023-03-15 21:30:10 +0100

    solved conflict between prettier and eslint

--- src/components/Modals/VisitModal/index.tsx
+++ src/components/Modals/VisitModal/index.tsx
@@ -1,13 +1,27 @@
+import { useForm } from "react-hook-form";
+
 interface VisitModalPropsInterface {
-  initState?: any
+  initState?: any;
 }
 
 const VisitModal = ({ initState }: VisitModalPropsInterface) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
   return (
     <div>
-      {/* <div>{title}</div> */}
+      <form>
+        <input defaultValue="" {...register("example")} />
+        <input {...register("exampleRequired", { required: true })} />
+        {/* errors will return when field validation fails  */}
+        {errors.exampleRequired != null && <span>This field is required</span>}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default VisitModal
+export default VisitModal;

3a9064e1f2abbd8f7bf7b86940f45b73b6e91152
Bartosz S <01169625@pw.edu.pl>
2023-05-09 15:56:50 +0200

    added missing files

---
+++ src/shared/utils/TokenUtils.ts
@@ -0,0 +1,18 @@
+import type JwtData from "@Interfaces/JwtData";
+import type Role from "@Types/Role";
+import jwt from "jwt-decode";
+
+const role = (): Role => {
+  const token = localStorage.getItem("token");
+  return token !== null ? jwt<JwtData>(token).role : "ANONYMOUS";
+};
+
+const saveToken = (token: string) => {
+  localStorage.setItem("token", token);
+};
+
+const deleteToken = () => {
+  localStorage.removeItem("token");
+};
+
+export { role, saveToken, deleteToken };
